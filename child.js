3bb46f17e5911439299e80aa71317d314362c43b
front-end-captain <18810940932@163.com>
2021-03-15 10:33:25 +0800

    chore:

--- child.js
+++ child.js
@@ -1,6 +1,28 @@
+const chalk = require("chalk");
 
 console.log("child id", process.pid);
+console.log();
 
 process.env.APP_ROLE_IS_CHILD = "APP_ROLE_IS_CHILD";
 
-console.log(process.env.APP_ROLE_IS_CHILD);
+console.log(chalk.bgBlue("before change parent process.env in child"));
+console.log();
+console.log("APP_ROLE_IS_PARENT", process.env.APP_ROLE_IS_PARENT);
+console.log();
+console.log("APP_ROLE_IS_CHILD", process.env.APP_ROLE_IS_CHILD);
+console.log();
+
+process.env.APP_ROLE_IS_PARENT = "APP_ROLE_IS_PARENT_CHANGED";
+
+console.log(chalk.bgBlue("after change parent process.env in child"));
+console.log();
+console.log("APP_ROLE_IS_PARENT", process.env.APP_ROLE_IS_PARENT);
+console.log();
+console.log("APP_ROLE_IS_CHILD", process.env.APP_ROLE_IS_CHILD);
+console.log();
+
+
+// process.send({ message: "from CHILD" });
+// process.on("message", (data) => {
+//   console.log("CHILD receive message", data);
+// });
--- parent.js
+++ parent.js
@@ -1,14 +1,24 @@
+/**
+ * `fork` 创建子进程时默认会将父进程的环境变量传递给子进程，所以在子进程可以访问父进程的环境变量(可读不可写)
+ */
+
 process.env.APP_ROLE_IS_PARENT = "APP_ROLE_IS_PARENT";
 
 console.log("parent id", process.pid);
+console.log();
 
 const { fork } = require("child_process");
 const chokidar = require("chokidar");
-
-const childPath = require.resolve("./child.js");
 const chalk = require("chalk");
 
-let child = fork(childPath);
+const childPath = require.resolve("./child.js");
+
+let child = fork(childPath, { env: {} });
+
+console.log("APP_ROLE_IS_PARENT", process.env.APP_ROLE_IS_PARENT);
+console.log();
+console.log("APP_ROLE_IS_CHILD", process.env.APP_ROLE_IS_CHILD);
+console.log();
 
 const watcher = chokidar.watch(".env*");
 
@@ -20,8 +30,30 @@ watcher.on("change", (filepath) => {
   console.log(chalk.yellow(`Try to restart dev server...`));
   console.log();
 
-  const changed_env = process.env;
+  console.log("APP_ROLE_IS_PARENT", process.env.APP_ROLE_IS_PARENT);
+  console.log();
+  console.log("APP_ROLE_IS_CHILD", process.env.APP_ROLE_IS_CHILD);
+  console.log();
 
   child.kill();
   child = fork(childPath, { env: {} });
+
+  console.log(chalk.bgBlue("after spawn child process"));
+  console.log();
+  console.log("APP_ROLE_IS_PARENT", process.env.APP_ROLE_IS_PARENT);
+  console.log();
+  console.log("APP_ROLE_IS_CHILD", process.env.APP_ROLE_IS_CHILD);
+  console.log();
+
+  // child.on("message", (data) => {
+  //   console.log("PARENT receive message", data);
+  // });
+
+  // child.send({ message: "from PARENT" });
 });
+
+// child.on("message", (data) => {
+//   console.log("PARENT receive message", data);
+// });
+
+// child.send({ message: "from PARENT" });
